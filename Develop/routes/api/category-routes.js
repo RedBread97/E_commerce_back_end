ab55123fa4c45989f2e818aeeb36e15e39c94b1e
Alex Lane <97alane@gmail.com>
2022-04-29 00:02:16 -0500

    tag and product models set up

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -46,10 +46,22 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+  const categoryData = await Category.update(req.body, {where: {id: req.params.id}});
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const categoryData = await Category.destroy(req.body, {where: {id: req.params.id}});
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(400).json(err)
+  }
 });
 
 module.exports = router;
