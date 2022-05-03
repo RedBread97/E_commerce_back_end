7c8a9d51e90f5d304075468a4e7d530fee25d9ba
Alex Lane <97alane@gmail.com>
2022-05-03 18:42:01 -0500

    get by id now works in tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,15 +6,33 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const productData = await Tag.findAll({
+      include: [ { model: Product, as: 'tags' }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const productData = await Tag.findByPk({
+      include: [ { model: Product, as: 'tags' }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new tag
+
 });
 
 router.put('/:id', (req, res) => {
