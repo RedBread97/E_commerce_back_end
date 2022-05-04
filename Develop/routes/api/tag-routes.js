09a5109b0b3bea32b374fc1e7a5f4d8c39fd69ee
Alex Lane <97alane@gmail.com>
2022-05-03 19:20:54 -0500

    tag route actually grabs info by id now

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const productData = await Tag.findByPk({
+    const productData = await Tag.findByPk(req.params.id, {
       include: [ { model: Product, as: 'tags' }],
     });
     res.status(200).json(productData);
